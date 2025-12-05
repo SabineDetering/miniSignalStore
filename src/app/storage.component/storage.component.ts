2ddded6008138fa499ae694bb1a90842445a8fb9
Sabine Detering <sabine.detering@BlueHands.de>
2025-12-05 19:40:53 +0100

    storage

--- src/app/storage.component/storage.component.ts
+++ src/app/storage.component/storage.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { BookWithStoredAmount, Id } from '../store/bookshop-types';
+import { BookShopStore } from '../store/bookshop.store';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-storage.component',
-  imports: [],
+  imports: [TableModule, ButtonModule],
   templateUrl: './storage.component.html',
   styleUrl: './storage.component.scss',
 })
-export class StorageComponent {
+export class StorageComponent {  
+  protected store = inject(BookShopStore);
 
+  protected stock= computed<Array<BookWithStoredAmount>>(() => 
+     this.store.books().map(book => ({
+        ...book,
+        storedAmount: this.store.stock()[book.id] || 0
+    })));
+  
+  // delivered(bookId: Id, amount: number){
+  //   this.store.dispatch(bookShopEvents.bookshopBookReceived({bookId: bookId, amount: amount}))
+  // }
 }
